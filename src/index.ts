f83afd14b1448973f3cdabc267c66ca92ffc4286
Benny Gebauer <bgebauer@headlessads.com>
2024-05-30 12:18:40 +0200

    adding security headers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
+import { secureHeaders } from "hono/secure-headers";
 import userRoutes from "api/v1/users/users.routes";
 
 const app = new Hono();
+
+// MIDDLEWARES
+app.use(secureHeaders());
 
 // ROUTES
 app.route("/v1/users", userRoutes);
