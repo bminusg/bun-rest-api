a103827ce75332bd263b5770869254d5c1a310b1
Benny Gebauer <bgebauer@headlessads.com>
2024-05-30 12:24:40 +0200

    adding rate limiter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { secureHeaders } from "hono/secure-headers";
+import limiter from "middlewares/limiter";
 import userRoutes from "api/v1/users/users.routes";
 
 const app = new Hono();
 
 // MIDDLEWARES
 app.use(secureHeaders());
+app.use(limiter);
 
 // ROUTES
 app.route("/v1/users", userRoutes);
---
+++ src/middlewares/limiter.ts
@@ -0,0 +1,10 @@
+import { rateLimiter } from "hono-rate-limiter";
+
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  keyGenerator: (c) => crypto.randomUUID(), // Method to generate custom identifiers for clients.
+});
+
+export default limiter;
